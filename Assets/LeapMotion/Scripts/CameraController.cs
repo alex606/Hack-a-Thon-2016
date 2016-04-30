60790996fd13618aa95fc43ad3c7701844fe25e4
Alexander Wu <alex606@att.net>
2016-04-30 11:06:36 -0700

    Added sounds

--- Assets/LeapMotion/Scripts/CameraController.cs
+++ Assets/LeapMotion/Scripts/CameraController.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
-
     public GameObject player;
     private Vector3 offset;
 
---
+++ Assets/LeapMotion/Scripts/PlaySound.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySound : MonoBehaviour {
+
+    private BoxCollider col;
+    private AudioSource audio;
+
+    void Start()
+    {
+        col = GetComponent<BoxCollider>();
+        audio = GetComponent<AudioSource>();
+    }
+
+
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("hello");
+        audio.Play();
+    }
+}
