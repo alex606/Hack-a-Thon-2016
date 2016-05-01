2ec1e319b6ac6bba524da957c8aab47a370c3b42
amarnathbh <abg55@mail.umkc.edu>
2016-04-30 23:11:36 -0500

    Scripts for app to start game
    Press enter to start an spp

---
+++ Assets/Scripts/MainMenuGuiScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.SceneManagement;
+
+public class MainMenuGuiScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 100, 200, 200));
+        GUILayout.FlexibleSpace();
+
+        if (GUILayout.Button("Start Game"))
+        {
+            StartGame();
+        }
+
+        if (GUILayout.Button("Press or Press EXC anytime during game to exit "))
+        {
+            EndGame();
+        }
+        GUILayout.FlexibleSpace();
+        GUILayout.EndArea();
+    }
+
+    void StartGame()
+    {
+     
+        SceneManager.LoadScene(1);
+    }
+
+    void EndGame()
+    {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+
+}
