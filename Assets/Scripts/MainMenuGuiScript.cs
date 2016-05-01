6744ddb8c872a1daeddaad285fb0568719fc648c
amarnathbh <abg55@mail.umkc.edu>
2016-05-01 01:33:46 -0500

    Changes to front filr

--- Assets/Scripts/MainMenuGuiScript.cs
+++ Assets/Scripts/MainMenuGuiScript.cs
@@ -22,14 +22,16 @@ public class MainMenuGuiScript : MonoBehaviour
     {
         GUILayout.BeginArea(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 100, 200, 200));
         GUILayout.FlexibleSpace();
-        GUI.Label(new Rect(10, 50, 50, 50), " Press EXC anytime to exit ");
+
 
         if (GUILayout.Button("Start Game"))
         {
             StartGame();
         }
-        
-            if (Input.GetKey("escape"))
+        GUI.contentColor = Color.black;
+        GUI.Label(new Rect(20, 50, 250, 75), "Press ESC anytime to Quit");
+
+        if (Input.GetKey("escape"))
         { 
                 Application.Quit();
 
