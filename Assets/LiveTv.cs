03393032c5ee0e0d63ac3e7de8c078d1c3aebb9b
Amarnathbh <b.h.amarnath@gmail.com>
2016-05-01 09:56:57 -0500

    Resolving git hysteria

---
+++ Assets/LiveTv.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(AudioSource))]
+
+public class LiveTv : MonoBehaviour {
+
+    public WWW wwwData;
+    public string url = "https://becunningandfulloftricks.files.wordpress.com/2013/04/hedgehog_in_the_fog.ogg";
+    public GUITexture gt;
+    void Start()
+    {
+        wwwData = new WWW(url);
+        gt = GetComponent<GUITexture>();
+        gt.texture = wwwData.movie;
+    }
+    void Update()
+    {
+        MovieTexture m = gt.texture as MovieTexture;
+        if (!m.isPlaying && m.isReadyToPlay)
+            m.Play();
+
+    }
+}
