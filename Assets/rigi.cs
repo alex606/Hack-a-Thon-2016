d5ce3b3d7486425140f62f0e84f6a6d31d7b0741
amarnathbh <abg55@mail.umkc.edu>
2016-04-30 23:13:10 -0500

    Added files via upload

---
+++ Assets/rigi.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rigi : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+
+    }
+}
